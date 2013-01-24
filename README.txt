88fbdfdbaf93df2c28c667b9e22d19a89f18fcfa
Willem van den Ende <willem@livingsoftware.nl>
2013-01-24 23:11:06 +0100

    list of items per cluster

--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 yak-cfd --start GUID --end GUID 
 iterates over a number of git commits, optionally limited by a commit
 range, collecting the number of vertices in a subgraph
+
+output in json for easier display in javascript libs ?
+
+like this:
+[{ CID : 1352434654b { "technical debt" : 50, "in progress" : 2,
+"global" : 30}] (and so on for each commit)
--- yakgraph.hs
+++ yakgraph.hs
@@ -1,11 +1,46 @@
 import Data.GraphViz.Types
 import Data.GraphViz.Types.Graph
 import Data.GraphViz.Commands.IO
+import Data.Map(keys,toAscList,empty,alter,Map(..))
+import Data.List(groupBy)
+import Data.Maybe
+import Data.Text.Lazy(unpack)
+import Data.Sequence(Seq)
+import Data.Foldable(toList)
 
 iograph = (readDotFile "planning.dot" :: IO (DotGraph String)) 
 
 smarthost g = successorsOf g "MailSmartHost"
 
+fromGraphId (Str s) = unpack s
+
+listOfClusterNames = map fromGraphId.map fromJust.keys. snd. graphStructureInformation
+
+keyByCluster ((n,(p,_)):ns) = (p,n) : keyByCluster ns
+keyByCluster []  =  []
+
+extractClusterName :: Seq (Maybe GraphID) -> String
+extractClusterName seq = case toList seq of
+                          []                  -> "inbox"
+                          (Just (Str name)):_ -> unpack name
+
+
+listOfPairsToMapOfKeysWithListOfValues ::  [(Seq(Maybe GraphID),String)] -> Map String [String]
+listOfPairsToMapOfKeysWithListOfValues pairs = groupStuff pairs empty
+  where
+    groupStuff :: [(Seq(Maybe GraphID),String)] -> Map String [String] -> Map String [String] 
+    groupStuff [] m = m
+    groupStuff ((c,n):rest) m =  alter (addToList n) (extractClusterName c) (groupStuff rest m)
+    addToList  n Nothing = Just [n]
+    addToList n (Just ns) = Just $ n:ns
+
+listOfNodesPerCluster =
+ listOfPairsToMapOfKeysWithListOfValues . concatMap keyByCluster .groupBy sameCluster. toAscList . nodeInformation False
+ where
+  sameCluster (n,(p,_)) (n',(p',_)) = p == p' 
+
 main = do 
     g <- iograph
-    return smarthost g
+    -- putStr $ show $ smarthost g
+    putStrLn $ show.listOfClusterNames $ g
+    putStrLn $ show.listOfNodesPerCluster $ g
