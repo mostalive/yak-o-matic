a16305d45ae09371c9490985f0be10e08aa55ca8
Willem van den Ende <willem@livingsoftware.nl>
2013-01-24 23:38:36 +0100

    one slice of data points for cfd

--- YakGraph.hs
+++ YakGraph.hs
@@ -1,21 +1,18 @@
-module YakGraph(listOfClusterNames, listOfNodesPerCluster) where
+module YakGraph(listOfClusterNames, listOfNodesPerCluster, countOfNodesPerCluster) where
 import Data.GraphViz.Types
 import Data.GraphViz.Types.Graph
 import Data.GraphViz.Commands.IO
-import Data.Map(keys,toAscList,empty,alter,Map(..))
+import qualified Data.Map as M
 import Data.List(groupBy)
 import Data.Maybe
 import Data.Text.Lazy(unpack)
 import Data.Sequence(Seq)
 import Data.Foldable(toList)
 
-iograph = (readDotFile "planning.dot" :: IO (DotGraph String)) 
-
-smarthost g = successorsOf g "MailSmartHost"
-
 fromGraphId (Str s) = unpack s
 
-listOfClusterNames = map fromGraphId.map fromJust.keys. snd. graphStructureInformation
+listOfClusterNames :: DotGraph String -> [String]
+listOfClusterNames = map fromGraphId.map fromJust.M.keys. snd. graphStructureInformation
 
 keyByCluster ((n,(p,_)):ns) = (p,n) : keyByCluster ns
 keyByCluster []  =  []
@@ -26,21 +23,21 @@ extractClusterName seq = case toList seq of
                           (Just (Str name)):_ -> unpack name
 
 
-listOfPairsToMapOfKeysWithListOfValues ::  [(Seq(Maybe GraphID),String)] -> Map String [String]
-listOfPairsToMapOfKeysWithListOfValues pairs = groupStuff pairs empty
+listOfPairsToMapOfKeysWithListOfValues ::  [(Seq(Maybe GraphID),String)] -> M.Map String [String]
+listOfPairsToMapOfKeysWithListOfValues pairs = groupStuff pairs M.empty
   where
     groupStuff [] m = m
-    groupStuff ((c,n):rest) m =  alter (addToList n) (extractClusterName c) (groupStuff rest m)
+    groupStuff ((c,n):rest) m =  M.alter (addToList n) (extractClusterName c) (groupStuff rest m)
     addToList  n Nothing = Just [n]
     addToList n (Just ns) = Just $ n:ns
 
+countOfNodesPerCluster :: DotGraph String -> [(String, Int)]
+countOfNodesPerCluster = map (\ (c,l) -> (c,length l)) . M.toList  .listOfNodesPerCluster
+
+
+listOfNodesPerCluster :: DotGraph String -> M.Map String [String]
 listOfNodesPerCluster =
- listOfPairsToMapOfKeysWithListOfValues . concatMap keyByCluster .groupBy sameCluster. toAscList . nodeInformation False
+ listOfPairsToMapOfKeysWithListOfValues . concatMap keyByCluster .groupBy sameCluster. M.toAscList . nodeInformation False
  where
   sameCluster (n,(p,_)) (n',(p',_)) = p == p' 
 
-main = do 
-    g <- iograph
-    -- putStr $ show $ smarthost g
-    putStrLn $ show.listOfClusterNames $ g
-    putStrLn $ show.listOfNodesPerCluster $ g
