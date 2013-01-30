92940f19678fd8bf7680239072bd897665862357
Arnaud Bailly <abailly@murex.com>
2013-01-30 21:59:36 +0100

    output list of graph's clusters count from a file's commit

--- yak.hs
+++ yak.hs
@@ -1,14 +1,30 @@
 import System.Environment
 import Data.GraphViz.Types.Graph
 import Data.GraphViz.Commands.IO
+import Data.GraphViz.Parsing
+import Data.Text.Lazy(pack)
+import Data.Maybe(fromJust)
+
 import YakGraph
+import YakGit
 
-iograph :: String -> IO (DotGraph String)
-iograph  = readDotFile
+iograph :: String -> DotGraph String
+iograph  = parseIt'.pack
 
+-- |Output cfddata for a given file in a given repo
+--
+-- >>> outputCfdData "test-repo" "planning.dot" >>= return . head
+-- "foo"
+outputCfdData :: FilePath        -- ^Path to git repo
+                 -> FilePath     -- ^File (expect graphviz format) to extract data from
+                 -> IO [String]  -- ^CFD Data extracted from graph file's content
+outputCfdData gitrepo filename = do
+  commits <- gitCommitsForFile gitrepo filename
+  contents <- mapM (gitContentOfFileAtCommit gitrepo filename . gitHash) commits 
+  return $ map (show . countOfNodesPerCluster . iograph . fromJust) contents
+
+  
 main = do
-    [repoGit,pathToFile] <- getArgs 
-    g <- iograph pathToFile
-    putStrLn $ show.listOfClusterNames $ g
-    putStrLn $ show.listOfNodesPerCluster $ g
-    putStrLn $ show.countOfNodesPerCluster $ g
+    [repoGit,pathToFile] <- getArgs
+    cfdData <- outputCfdData repoGit pathToFile
+    mapM_ putStrLn cfdData
