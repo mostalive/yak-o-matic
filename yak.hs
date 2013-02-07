22726fe67c967ab8cf09558d199690d47e8af7b9
Arnaud Bailly <arnaud@foldlabs.com>
2013-02-07 07:23:32 +0100

    debug content of read .dot file with -v option

--- yak.hs
+++ yak.hs
@@ -30,29 +30,35 @@ emptyOptions = YakOptions False Nothing Nothing
 -- >>> makeOptions ["foo" ,"-v", "bar"]
 -- YakOptions {debugParsing = True, gitRepository = Just "foo", relativeYakFilePath = Just "bar"}
 makeOptions :: [String] -> YakOptions 
-makeOptions opts = makeOptions' opts emptyOptions
+makeOptions options = makeOptions' options emptyOptions
   where
     makeOptions' ("-v":opts) y                                = makeOptions' opts $ y { debugParsing = True }
     makeOptions' (opt:opts)  y@(YakOptions _ Nothing Nothing) = makeOptions' opts $ y { gitRepository = Just opt }
     makeOptions' (opt:opts)  y@(YakOptions _ _       Nothing) = makeOptions' opts $ y { relativeYakFilePath = Just opt }
-    makeOptions' (opt:opts)  y@(YakOptions _ _       _)       = makeOptions' opts $ y 
+    makeOptions' (_:opts)    y@(YakOptions _ _       _)       = makeOptions' opts $ y 
     makeOptions' []          y                                = y 
   
 -- |Output cfddata for a given file in a given repo
 --
 -- >>> outputCfdData (YakOptions False (Just "test-repo") (Just "planning.dot")) >>= return . head
 -- "[(\"done\",13),(\"inbox\",9),(\"inprogress\",2),(\"processacceleration\",4),(\"technicaldebt\",6)]"
+--
 -- >>> outputCfdData (YakOptions True (Just "test-repo") Nothing)
 -- *** Exception: Invalid command-line arguments
 outputCfdData :: YakOptions    
                  -> IO [String]  -- ^CFD Data extracted from graph file's content
-outputCfdData (YakOptions _ (Just gitrepo) (Just filename)) =
+outputCfdData (YakOptions debug (Just gitrepo) (Just filename)) =
   gitCommitsForFile gitrepo filename >>=
   mapM (gitContentOfFileAtCommit gitrepo filename . gitHash) >>=
+  maybeDebug debug >>=
   return . map (show . countOfNodesPerCluster . iograph . fromJust)
 outputCfdData _ = error "Invalid command-line arguments"
 
   
+maybeDebug :: Bool -> [Maybe String] -> IO ([Maybe String])
+maybeDebug True s  = putStrLn (show s) >> return s
+maybeDebug False s = return s
+
 main :: IO ()
 main = do
     args <- getArgs
