09e80713a5799afe4cdb7196a6ced49da1f4e660
Arnaud Bailly <arnaud@foldlabs.com>
2013-02-07 07:13:56 +0100

    introduced option for debugging .dot parsing

--- yak.hs
+++ yak.hs
@@ -10,21 +10,52 @@ import YakGit
 iograph :: String -> DotGraph String
 iograph  = parseIt'.pack
 
+data YakOptions = YakOptions {
+  debugParsing         :: Bool,            -- ^Trace Graphviz parser input & output
+  gitRepository        :: Maybe FilePath,  -- ^Path to git repo                               
+  relativeYakFilePath  :: Maybe FilePath   -- ^File (expect graphviz format) to extract data from
+  } deriving (Eq, Show, Read)
+             
+emptyOptions :: YakOptions
+emptyOptions = YakOptions False Nothing Nothing
+
+-- |Parse command-line arguments building options for yakgraph execution
+--
+-- >>> makeOptions []
+-- YakOptions {debugParsing = False, gitRepository = Nothing, relativeYakFilePath = Nothing}
+--
+-- >>> makeOptions ["foo" ,"bar"]
+-- YakOptions {debugParsing = False, gitRepository = Just "foo", relativeYakFilePath = Just "bar"}
+--
+-- >>> makeOptions ["foo" ,"-v", "bar"]
+-- YakOptions {debugParsing = True, gitRepository = Just "foo", relativeYakFilePath = Just "bar"}
+makeOptions :: [String] -> YakOptions 
+makeOptions opts = makeOptions' opts emptyOptions
+  where
+    makeOptions' ("-v":opts) y                                = makeOptions' opts $ y { debugParsing = True }
+    makeOptions' (opt:opts)  y@(YakOptions _ Nothing Nothing) = makeOptions' opts $ y { gitRepository = Just opt }
+    makeOptions' (opt:opts)  y@(YakOptions _ _       Nothing) = makeOptions' opts $ y { relativeYakFilePath = Just opt }
+    makeOptions' (opt:opts)  y@(YakOptions _ _       _)       = makeOptions' opts $ y 
+    makeOptions' []          y                                = y 
+  
 -- |Output cfddata for a given file in a given repo
 --
--- >>> outputCfdData "test-repo" "planning.dot" >>= return . head
+-- >>> outputCfdData (YakOptions False (Just "test-repo") (Just "planning.dot")) >>= return . head
 -- "[(\"done\",13),(\"inbox\",9),(\"inprogress\",2),(\"processacceleration\",4),(\"technicaldebt\",6)]"
-outputCfdData :: FilePath        -- ^Path to git repo
-                 -> FilePath     -- ^File (expect graphviz format) to extract data from
+-- >>> outputCfdData (YakOptions True (Just "test-repo") Nothing)
+-- *** Exception: Invalid command-line arguments
+outputCfdData :: YakOptions    
                  -> IO [String]  -- ^CFD Data extracted from graph file's content
-outputCfdData gitrepo filename =
+outputCfdData (YakOptions _ (Just gitrepo) (Just filename)) =
   gitCommitsForFile gitrepo filename >>=
   mapM (gitContentOfFileAtCommit gitrepo filename . gitHash) >>=
   return . map (show . countOfNodesPerCluster . iograph . fromJust)
+outputCfdData _ = error "Invalid command-line arguments"
 
   
 main :: IO ()
 main = do
-    [repoGit,pathToFile] <- getArgs
-    cfdData <- outputCfdData repoGit pathToFile
+    args <- getArgs
+    let yakOptions = makeOptions args
+    cfdData <- outputCfdData yakOptions
     mapM_ putStrLn cfdData
